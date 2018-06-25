5e3e3ea07d26f6ee482258468166f16f1797288f
DamienPoix <damienpoix@live.fr>
2018-06-25 14:58:11 +0200

    Exercice 3 Part.3

---
+++ assets/js/fonction.js
@@ -0,0 +1,10 @@
+//fonction pour changer la couleur
+function bGreen(){
+  document.getElementById('text').style.color = 'green';
+}
+function bRed(){
+  document.getElementById('text').style.color = 'red';
+}
+function bBlue(){
+    document.getElementById('text').style.color = 'blue';
+}
